28669b0d7c5974d95676c1814b5d0bff18a90443
hamstar0 <webutek@gmail.com>
2022-05-12 03:31:55 -0700

    v2.1.0.5 - Restored some noise to PKE reading

--- FindableManaCrystals/MyConfig.cs
+++ FindableManaCrystals/MyConfig.cs
@@ -143,9 +143,9 @@ namespace FindableManaCrystals {
 		////
 
 		[Range( 0f, 1f )]
-		[DefaultValue( 1f / 24f )]	// was 1/32
+		[DefaultValue( 1f / 28f )]	// was 1/32
 		[CustomModConfigItem( typeof(MyFloatInputElement) )]
-		public float PKEDetectChancePerTick { get; set; } = 1f / 24f;
+		public float PKEDetectChancePerTick { get; set; } = 1f / 28f;
 
 		[DefaultValue( true )]
 		public bool PKEDetectInterference { get; set; } = true;
